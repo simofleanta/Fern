9fc7b6e18687a5bf1343eac5009af8848f2d29df
simofleanta <simo.fleanta@gmail.com>
2020-12-17 13:51:49 +0100

    a

---
+++ F.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+
+
+c=pd.read_csv('costs.csv')
+print(c.columns)
+df=DataFrame(c.head(500))
+print(df.head(500))
+
