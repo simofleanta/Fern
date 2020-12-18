862342d7459f1ee9c99cd01149d5291e13a7c46b
simofleanta <simo.fleanta@gmail.com>
2020-12-18 12:04:46 +0100

    s

--- opperational.py
+++ opperational.py
@@ -50,3 +50,43 @@ print(Opperational_view.mean())
 
 Monthly_view=odf.groupby(['Month'])[['Costs']]
 print(Monthly_view.mean())
+
+#Where is the money?
+
+#highest costs in months
+highest_costs=odf.groupby(["Opp_costs", "Month"]).sum().sort_values(["Costs"],ascending=False)
+print(highest_costs)
+
+opp_costs=odf.groupby(["Opp_costs"]).sum().sort_values(["Costs"],ascending=False)
+print(opp_costs)
+
+#hottet month
+
+Hot_month=odf.groupby(["Month"]).sum().sort_values(["Costs"],ascending=False)
+print(Hot_month)
+
+#pie charts
+
+o=odf
+df = px.data.tips()
+fig = px.pie(o, values='Costs', names='Opp_costs', color_discrete_sequence=px.colors.sequential.Blues)
+plotly.offline.plot(fig, filename='o')
+
+#Hottest months
+o=odf
+df = px.data.tips()
+fig = px.pie(o, values='Costs', names='Month', color_discrete_sequence=px.colors.sequential.Blues)
+plotly.offline.plot(fig, filename='o')
+
+#Bar chart
+
+odf_cost=odf.groupby(['Opp_costs'])['Costs'].mean()
+cost=pd.DataFrame(data=odf_cost)
+cost_bar=cost.sort_values(by='Costs',ascending=False,axis=0)
+
+fig = px.bar(cost_bar, x="Costs", y=cost_bar.index, color='Costs',color_continuous_scale='Blues',title="Cost per opperations")
+plotly.offline.plot(fig, filename='o')
+
+
+
+
