195ebf3c8d3d242ac912f1d20af3de3d8ead2274
simofleanta <simo.fleanta@gmail.com>
2020-12-18 11:32:25 +0100

    save opperational doc

---
+++ opperational.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+from datetime import datetime
+from matplotlib import rcParams
+
+#open file
+o=pd.read_csv('opp.csv')
+print(o.columns)
+odf=DataFrame(o.head(50))
+print(odf.head(53))
+#b=df.dtypes
